59ae752b5ddc380fcb1423769329c674ce7dc0a4
zhou0123 <zhou812986561@gmail.com>
2022-08-09 20:50:13 +0800

    1

--- array/array.cpp
+++ array/array.cpp
@@ -1,8 +1,11 @@
 
 #include <stdarg.h>
 #include <immintrin.h>
+#include <xmmintrin.h>
 #include<iostream>
- #include<initializer_list>
+#include<initializer_list>
+#include <sched.h>
+#include<thread>
 using namespace std;
 
 template <class T>
@@ -10,25 +13,28 @@ int len(T &array)
 {
     return  sizeof(array)/(array[0]);
 }
-class  array
+class  ndarray
 {
+
 private:
-    int *shape;
     int num{0};//元素个数
-    float *nums;//储存元素
     int len_shape;//shape的len
     float* sum_dim_x ();
     float* sum_dim_0();
     float* sum_dim_1();
 public:
-     array(initializer_list<int> il);
-    ~ array();
+    int *shape;
+    float *nums;//储存元素
+     ndarray(initializer_list<int> il);
+    ~ ndarray();
     void put_out();
     void construct(initializer_list<float> il);
+    void transpose();
     float *sum(int dim =-1);
     int len();
-
-    array &operator+=(float x)
+    void dot(ndarray &other);
+    
+    ndarray &operator+=(float x)
     {
         #pragma omp simd
         for (int i=0;i<num;i++)
@@ -38,7 +44,7 @@ public:
 
         return *this;
     }
-    array &operator+ (float x)
+    ndarray &operator+ (float x)
     {
         #pragma omp simd 
         for(int i=0;i<num;i++)
@@ -47,7 +53,7 @@ public:
         }
         return *this;
     }
-    array &operator-=(float x)
+    ndarray &operator-=(float x)
     {
         #pragma omp simd
         for (int i=0;i<num;i++)
@@ -58,7 +64,7 @@ public:
         return *this;
 
     }
-    array &operator -(float x)
+    ndarray &operator -(float x)
     {
         #pragma omp simd 
         for(int i=0;i<num;i++)
@@ -67,7 +73,7 @@ public:
         }
         return *this;
     }
-    array &operator*=(float x)
+    ndarray &operator*=(float x)
     {
         #pragma omp simd
         for (int i=0;i<num;i++)
@@ -78,7 +84,7 @@ public:
         return *this;
 
     }
-    array &operator *(float x)
+    ndarray &operator *(float x)
     {
         #pragma omp simd 
         for(int i=0;i<num;i++)
@@ -87,7 +93,7 @@ public:
         }
         return *this;
     }
-    array &operator/=(float x)
+    ndarray &operator/=(float x)
     {
         #pragma omp simd
         for (int i=0;i<num;i++)
@@ -98,7 +104,7 @@ public:
         return *this;
 
     }
-    array &operator /(float x)
+    ndarray &operator /(float x)
     {
         #pragma omp simd 
         for(int i=0;i<num;i++)
@@ -106,13 +112,66 @@ public:
             nums[i]-=x;
         }
         return *this;
+    }
+    float* &operator [](initializer_list<int> il)
+    {
+        int size_ = il.size();
+        if (size_>2)
+        {
+            cout<<"error"<<endl;
+            return;
+        }
+        
+        else if(size_ ==1) 
+        {
+            
+
+            int loc = *il.begin();
+            if (len_shape ==1) 
+            {
+                float* res =new float[size_];
+                res[0] = nums[loc];
+                return res;
+            }
+            else if (len_shape ==2)
+            {
+                float* res =new float[shape[1]];
+                #pragma omp simd
+                for (int i=0;i<shape[1];i++)
+                {
+                    res[i] = nums[i+loc*shape[1]];
+                }
+
+                return res;
+            }
+        }
+        else if(size_ ==2)
+        {
+            int i=0;
+            int second;
+            for (auto itor = il.begin();itor!=il.end();itor++)
+            {
+                if (*itor >=shape[i])
+                {
+                    cout<<"error"<<endl;
+                    return ;
+                }
+                second = *itor;
+                i++;
+            }
+
+            float * res = new float();
+
+            *res = nums[shape[0]**((il.begin()))+second];
+            return res;
+        } 
     }
 };
-int array::len()
+int ndarray::len()
 {
     return len_shape;
 }
- array:: array(initializer_list<int> il)
+ ndarray:: ndarray(initializer_list<int> il)
 {
     int size_ = il.size();
     shape = new int[size_];
@@ -124,7 +183,7 @@ int array::len()
         i++;
     }
 }
-void array::construct(initializer_list<float> il)
+void ndarray::construct(initializer_list<float> il)
 {
     
     nums = new float[il.size()];
@@ -137,7 +196,7 @@ void array::construct(initializer_list<float> il)
         num+=1;
     }
 }
-float* array::sum_dim_x()
+float* ndarray::sum_dim_x()
 {
      float *result =new float();
         if (num >=4)
@@ -164,7 +223,7 @@ float* array::sum_dim_x()
         for (int i=((num)/4)*4;i<num;i++)*result += nums[i];
         return result;
 }
-float* array::sum_dim_0()
+float* ndarray::sum_dim_0()
 {
     float * result = new float[shape[1]]();
     if (shape[1]<4)
@@ -204,7 +263,7 @@ float* array::sum_dim_0()
     }
     return result;
 }
-float* array::sum_dim_1()
+float* ndarray::sum_dim_1()
 {
     float * result = new float[shape[0]]();
 
@@ -218,7 +277,7 @@ float* array::sum_dim_1()
     }
     return result;
 }
-float* array::sum(int dim)
+float* ndarray::sum(int dim)
 {
     if (dim ==-1)
     {
@@ -237,11 +296,68 @@ float* array::sum(int dim)
     }
     
 }
-// float* dot(array& other)
-// {
-    
-// }
-void array::put_out()
+void ndarray::dot(ndarray& other)
+{
+    int * other_shape;
+    other_shape = other.shape;
+    int num_tmp = shape[0]*other_shape[1];
+    float* res = new float[num_tmp]();
+
+    #pragma omp simd
+    int core_num =8;
+    std::thread workers[8];
+    // std::thread workers[other_shape[1]];
+    int base_size = other_shape[1]/8;
+    int remain = other_shape[1]%8;
+    for (int num_thread=0;num_thread<8;num_thread++)
+    {
+        // if (remain!=0)
+        // {
+        //     int start =num_thread*base_size;
+        //     int end = (num_thread+1)*base_size+1;
+        //     remain--;
+        // }
+        workers[num_thread] = std::thread(
+            [&]{
+                
+                for (int j=num_thread*base_size;j<(num_thread+1)*base_size;j++)
+                {
+                    for (int i=0;i<shape[0];i++)
+                    {
+                        for (int k=0;k<shape[1];k++)res[j+shape[0]*i] += nums[i*shape[0]+k]*other.nums[k*other_shape[0]+j];
+                    }
+                }
+                
+            }
+
+        );
+        //绑core
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(num_thread, &cpuset);
+        int rc = pthread_setaffinity_np(workers[num_thread].native_handle(), sizeof(cpu_set_t), &cpuset);
+    }
+    for (auto & worker : workers) worker.join();
+
+    if (remain !=0)
+    {
+        #pragma omp parallal num_threads(remain)
+        {
+            #pragma omp for
+            {
+                for (int j=8*base_size;j<other_shape[1];j++)
+                {
+                    for (int i=0;i<shape[0];i++)
+                    {
+                        for (int k=0;k<shape[1];k++)res[j+shape[0]*i] += nums[i*shape[0]+k]*other.nums[k*other_shape[0]+j];
+                    }
+                }
+
+            } 
+        }
+    }
+}
+void ndarray::put_out()
 {
     for(int i=0;i<shape[1];i++)
     {
@@ -250,14 +366,30 @@ void array::put_out()
     
     }
 }
- array::~ array()
+ ndarray::~ ndarray()
 {
 }
+void ndarray::transpose()
+{
+    float * nums_ = new float[num];
+    #pragma omp parallal for 
+    for(int i=0;i<shape[0];i++)
+    {
+        for (int j=0;j<shape[1];j++)
+        {
+            nums_[i+shape[1]*j] = nums[i*shape[0]+j];
+        }
+    }
 
-
+    delete nums;
+    nums = nums_;
+    int tmp=shape[1];
+    shape[1] = shape[0];
+    shape[0] = shape[1];
+}
 int main()
 {
-    array nu={2,2};
+    ndarray nu={2,2};
     nu.construct({1,2,3,4});
     float *a = new float[4];
     a = nu.sum(1);
--- array/test.cpp
+++ array/test.cpp
@@ -1,34 +1,23 @@
  #include<initializer_list>
  #include<iostream>
+ #include<thread>
  using namespace std;
- double sum(initializer_list<double> il)
- {
-     double sum=0;
-     for(auto tmp : il) sum+=tmp;
-     return sum;
- }
- 
- double average(const initializer_list<double> &ril)
- {
-     if(ril.size()>0)
-     {
-         double sum=0;
-         for(auto tmp : ril) sum+=tmp;
-         return sum/ril.size();
-     }
-     return 0.0;
- }
+int main()
+{
+    int count =0;
 
-//  int main()
-//  {
-//      cout << "sum:" << sum({2,3,4})
-//          << ", ave:" << average({2.0,3,4}) << endl;
-//      initializer_list<double> dl={1.5,2.5,3.5,4.5,5.5};
-//      cout << "sum:" << sum(dl)
-//          << ", ave:" << average(dl) << endl;
-//      dl={1,2,3,4,5,6};
-//      cout << "sum:" << sum(dl)
-//          << ", ave:" << average(dl) << endl;
-//  }
- 
- 
+    std::thread workers[8];
+
+    for (int i=0;i<8;i++)
+    {
+        workers[i] = std::thread([&]
+        {
+            count++;
+            cout<<"..."<<count<<"..."<<endl;
+        });
+    }
+
+    for (auto & worker :workers) worker.join();
+    cout<<count<<endl;
+    return 0 ;
+}
